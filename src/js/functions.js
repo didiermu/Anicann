142f5dfc520fc0273acfd459fcf8a1626f6b0bed
Didier Muñiz Estevez <didier.m@bnn.mx>
2020-06-15 17:37:00 -0500

    aniamciones

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,9 +1,59 @@
+
 $(document).ready(function () {
     $("html").css("opacity", "1");
 
-    $('#modal-newsletter').modal('show')
+    // $('#modal-newsletter').modal('show')
 
 
+     /// ANIMACIONES
+
+
+     function efectoFade(inicioFade) {
+
+         inicioFade = 200;
+
+         setTimeout(function () {
+             $(".home--hero--copy h1").addClass("transition1");
+             if ($(window).height() > 700) {
+
+                 $(".row.objetFade").addClass("transition1");
+                 $(".noticias--notas .objetFade").addClass("transition1");
+                 
+                 $(document).scroll(function () {
+                     $(".home--noticias--notas__nota").addClass("transition1");
+                     $(".noticias--notas .objetFade").addClass("transition1");
+                 });
+            }
+         }, 500);
+         
+
+        
+
+        function sectionFade() {
+            var sectionFade = $(".sectionFade");
+            sectionFade.each(function () {
+                var posSection = $(this).offset().top - 500;
+                var scrolleo = $(window).scrollTop();
+
+                if (scrolleo > posSection) {
+                    $(this).find($(".objetFade")).addClass("transition1");
+                } else {
+                    $(this).find($(".objetFade")).removeClass("transition1");
+                }
+
+            });
+        }
+
+        sectionFade();
+
+        $(document).scroll(function() {
+            sectionFade();
+        });
+     }
+    
+    efectoFade();
+
+    
     // HEADER
 
     $(".header--navegacion .btn").on("click", function () {
